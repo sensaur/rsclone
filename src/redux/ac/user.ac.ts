b4a06627c6560794eeebe56d90ef1fc5ae1beb50
Ilya Zhigarev <zhigarev@bk.ru>
2023-02-03 18:08:20 +0100

    fix: clean code

--- src/redux/ac/user.ac.ts
+++ src/redux/ac/user.ac.ts
@@ -4,7 +4,6 @@ import { setUserSlice } from '../slices/userSlice';
 
 // eslint-disable-next-line import/prefer-default-export,consistent-return
 export const signIn = (payload: any, navigate: any) => async (dispatch: any) => {
-  console.log('222', endPoints.signIn());
   const response = await fetch(endPoints.signIn(), {
     method: 'POST',
     headers: {
@@ -16,15 +15,6 @@ export const signIn = (payload: any, navigate: any) => async (dispatch: any) => 
   if (response.status === 200) {
     const user = await response.json();
     dispatch(setUserSlice(user));
-    // if (user.role === 'admin') {
-    //   return navigate('/claims');
-    // }
-    // if (user.role === 'manager') {
-    //   return navigate('/claimsManager');
-    // }
-    // if (user.role === 'client') {
-    //   return navigate('/claimsClient');
-    // }
     return navigate('/');
   }
   Swal.fire('Неправильный логин / пароль');
