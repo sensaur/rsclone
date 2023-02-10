50aba94db056ccb89a233d3e61ecad0ca7437939
nikola.balabanov <nikola.balabanov93@gmail.com>
2023-02-10 18:05:44 +0300

    fix: remove unused links

--- src/App.tsx
+++ src/App.tsx
@@ -2,7 +2,6 @@ import React from 'react';
 import './App.css';
 import { Routes, Route } from 'react-router-dom';
 import { useAppSelector } from './hooks/redux';
-import Empty from './components/Empty';
 import SignIn from './components/SignIn';
 import SignUp from './components/SignUp';
 import NavBarStart from './components/NavBarStart';
@@ -14,7 +13,6 @@ function App() {
     <>
       {!user ? <NavBarStart /> : null}
       <Routes>
-        <Route path="/" element={<Empty />} />
         <Route path="/login" element={<SignIn />} />
         <Route path="/signup" element={<SignUp />} />
       </Routes>
