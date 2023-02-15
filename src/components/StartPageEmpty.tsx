98ca1711f3692929181b6e97472251ec518dfd42
nikola.balabanov <nikola.balabanov93@gmail.com>
2023-02-15 10:35:33 +0300

    eslint: fix

--- src/components/StartPageEmpty.tsx
+++ src/components/StartPageEmpty.tsx
@@ -1,5 +1,7 @@
 import { Swiper, SwiperSlide } from 'swiper/react';
-import { Navigation, Pagination, Scrollbar, A11y } from 'swiper';
+import {
+  Navigation, Pagination, Scrollbar, A11y,
+} from 'swiper';
 import Tslogo from '../images/TSlogo.png';
 import ReactLogo from '../images/Reactlogo.png';
 import TwLogo from '../images/TWlogo.jpg';
@@ -11,10 +13,9 @@ import VladAv from '../images/VladAv.png';
 import 'swiper/css';
 import 'swiper/css/navigation';
 
-
 function StartPageEmpty() {
   return (
-    <div >
+    <div>
       <h1 className="md:text-2xl text-base text-center text-color1">What this app can do?</h1>
       <ul className="text-center pt-1">
         <li className="md:text-xl text-xs text-color1">- Create as many boards as you wish</li>
@@ -24,7 +25,7 @@ function StartPageEmpty() {
         <li className="md:text-xl text-xs text-color1">- Drag and drop your tasks both inside and into other columns as you wish</li>
       </ul>
       <h2 className="md:text-2xl text-base text-center text-color1 md:pt-4 pt-1">This app is powered by:</h2>
-      <div className='max-w-[50%] mx-auto md:pt-4 pt-1' >
+      <div className="max-w-[50%] mx-auto md:pt-4 pt-1">
         <Swiper
           modules={[Navigation, Pagination, Scrollbar, A11y]}
           spaceBetween={0}
@@ -35,66 +36,66 @@ function StartPageEmpty() {
           onSlideChange={() => console.log('slide change')}
         >
           <SwiperSlide>
-            <div className='flex flex-col text-center'>
-              <a href='https://www.typescriptlang.org/'>
-                <img className='w-4/12 h-4/12 mx-auto rounded-full' src={Tslogo} alt="TS" />
+            <div className="flex flex-col text-center">
+              <a href="https://www.typescriptlang.org/">
+                <img className="w-4/12 h-4/12 mx-auto rounded-full" src={Tslogo} alt="TS" />
               </a>
-              <p className='md:pt-4 pt-1 text-color1 md:text-xl text-xs'>TypeScript</p>
+              <p className="md:pt-4 pt-1 text-color1 md:text-xl text-xs">TypeScript</p>
             </div>
           </SwiperSlide>
           <SwiperSlide>
-            <div className='flex flex-col text-center'>
-              <a href='https://reactjs.org/'>
-                <img className='w-4/12 h-4/12 mx-auto rounded-full' src={ReactLogo} alt="React" />
+            <div className="flex flex-col text-center">
+              <a href="https://reactjs.org/">
+                <img className="w-4/12 h-4/12 mx-auto rounded-full" src={ReactLogo} alt="React" />
               </a>
-              <p className='md:pt-4 pt-1 text-color1 md:text-xl text-xs'>React</p>
+              <p className="md:pt-4 pt-1 text-color1 md:text-xl text-xs">React</p>
             </div>
           </SwiperSlide>
           <SwiperSlide>
-            <div className='flex flex-col text-center'>
-              <a href='https://tailwindcss.com'>
-                <img className='w-4/12 h-4/12 mx-auto rounded-full' src={TwLogo} alt="TW" />
+            <div className="flex flex-col text-center">
+              <a href="https://tailwindcss.com">
+                <img className="w-4/12 h-4/12 mx-auto rounded-full" src={TwLogo} alt="TW" />
               </a>
-              <p className='md:pt-4 pt-1 text-color1 md:text-xl text-xs'>TailWind</p>
+              <p className="md:pt-4 pt-1 text-color1 md:text-xl text-xs">TailWind</p>
             </div>
           </SwiperSlide>
           <SwiperSlide>
-            <div className='flex flex-col text-center'>
-              <a href='https://nodejs.org/en/' >
-                <img className='w-4/12 h-4/12 mx-auto rounded-full' src={NodeLogo} alt="Node" />
+            <div className="flex flex-col text-center">
+              <a href="https://nodejs.org/en/">
+                <img className="w-4/12 h-4/12 mx-auto rounded-full" src={NodeLogo} alt="Node" />
               </a>
-              <p className='md:pt-4 pt-1 text-color1 md:text-xl text-xs'>NodeJS</p>
+              <p className="md:pt-4 pt-1 text-color1 md:text-xl text-xs">NodeJS</p>
             </div>
           </SwiperSlide>
           <SwiperSlide>
-            <div className='flex flex-col text-center'>
-              <a href='https://www.postgresql.org/' >
-                <img className='w-4/12 h-4/12 mx-auto rounded-full' src={PostLogo} alt="Post" />
+            <div className="flex flex-col text-center">
+              <a href="https://www.postgresql.org/">
+                <img className="w-4/12 h-4/12 mx-auto rounded-full" src={PostLogo} alt="Post" />
               </a>
-              <p className='md:pt-4 pt-1 text-color1 md:text-xl text-xs'>PostgreSQL</p>
+              <p className="md:pt-4 pt-1 text-color1 md:text-xl text-xs">PostgreSQL</p>
             </div>
           </SwiperSlide>
         </Swiper>
       </div>
       <h2 className="md:text-2xl text-base text-center text-color1 md:pt-4 pt-1">Developers:</h2>
-      <div className='flex justify-center md:pt-5 pt-1'>
+      <div className="flex justify-center md:pt-5 pt-1">
         <div>
-          <a href='https://github.com/sensaur' >
-            <img className='w-4/12 h-4/12 mx-auto rounded-full' src={IlyaAv} alt="Post" />
+          <a href="https://github.com/sensaur">
+            <img className="w-4/12 h-4/12 mx-auto rounded-full" src={IlyaAv} alt="Post" />
           </a>
-          <p className='md:pt-4 pt-1 text-color1 md:text-xl text-xs text-center'>Ilya Zhigarev</p>
+          <p className="md:pt-4 pt-1 text-color1 md:text-xl text-xs text-center">Ilya Zhigarev</p>
         </div>
         <div>
-          <a href='https://github.com/NikolayBalabanov' >
-            <img className='w-4/12 h-4/12 mx-auto rounded-full' src={KolyaAv} alt="Post" />
+          <a href="https://github.com/NikolayBalabanov">
+            <img className="w-4/12 h-4/12 mx-auto rounded-full" src={KolyaAv} alt="Post" />
           </a>
-          <p className='md:pt-4 pt-1 text-color1 md:text-xl text-xs text-center'>Nikolay Balabanov</p>
+          <p className="md:pt-4 pt-1 text-color1 md:text-xl text-xs text-center">Nikolay Balabanov</p>
         </div>
         <div>
-          <a href='https://github.com/VEKozlov111' >
-            <img className='w-4/12 h-4/12 mx-auto rounded-full' src={VladAv} alt="Post" />
+          <a href="https://github.com/VEKozlov111">
+            <img className="w-4/12 h-4/12 mx-auto rounded-full" src={VladAv} alt="Post" />
           </a>
-          <p className='md:pt-4 pt-1 text-color1 md:text-xl text-xs text-center'>Vlad Kozlov</p>
+          <p className="md:pt-4 pt-1 text-color1 md:text-xl text-xs text-center">Vlad Kozlov</p>
         </div>
       </div>
     </div>
