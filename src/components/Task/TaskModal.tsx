e6e22813994d0cffb3172f9d28dfcadefb5a080e
VEKozlov111 <vlad.kozlov.2015@gmail.com>
2023-02-24 00:39:17 +0300

    feat: fix some bugs with headr, modals and card header

--- src/components/Task/TaskModal.tsx
+++ src/components/Task/TaskModal.tsx
@@ -110,7 +110,7 @@ function TaskModal({
     >
       <div
         ref={modalContent}
-        className="w-6/12 flex flex-col rounded-lg relative shadow-md bg-white px-6 py-3 -translate-y-10 duration-300 transition-transform dark:bg-colorD1"
+        className="w-10/12  md:w-6/12 flex flex-col rounded-lg relative shadow-md bg-white px-6 py-3 -translate-y-10 duration-300 transition-transform dark:bg-colorD1"
       >
         <div className="flex flex-col-reverse justify-between items-center pb-4">
           <h3 className="text-3xl font-semibold self-start dark:text-colorD3">
