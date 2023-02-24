f16ebbfc0f6a7f0312d1fcdaf663760435833ffd
nikola.balabanov <nikola.balabanov93@gmail.com>
2023-02-24 15:52:12 +0300

    feat: rounded scroll

--- src/components/Columns/ColumnsWraper.tsx
+++ src/components/Columns/ColumnsWraper.tsx
@@ -12,7 +12,14 @@ function ColumnsWraper(props: Tprops) {
     <Droppable droppableId="columns" direction="horizontal" type="column">
       {(provided) => (
         <div
-          className="flex gap-4 overflow-x-auto"
+          className="
+            flex
+            gap-4
+            overflow-x-scroll
+            scrollbar-thin
+            scrollbar-thumb-rounded-md
+            scrollbar-track-rounded-md
+          "
           {...provided.droppableProps}
           ref={provided.innerRef}
         >
